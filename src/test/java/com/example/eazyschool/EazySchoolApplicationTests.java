4fea286ff13394d93d1f1a3e3e829e751f87826e
weerapperuma <weerapperumasahiru@gmail.com>
2025-04-23 16:56:55 +0530

    Initial commit

---
+++ src/test/java/com/example/eazyschool/EazySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazySchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3037412b0a88331e263290f0c37fbe3fd9422cc9
weerapperuma <weerapperumasahiru@gmail.com>
2025-05-15 15:14:18 +0530

    add spring validation

--- src/main/java/com/example/eazyschool/controller/ContactController.java
+++ src/main/java/com/example/eazyschool/controller/ContactController.java
@@ -2,13 +2,19 @@ package com.example.eazyschool.controller;
 
 import com.example.eazyschool.model.Contact;
 import com.example.eazyschool.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @Controller
 public class ContactController {
     private final ContactService contactService;
@@ -17,13 +23,22 @@ public class ContactController {
     public ContactController(ContactService contactService){
         this.contactService=contactService;
     }
+
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
+
     @RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        contactService.setCounter(contactService.getCounter()+1);
+        log.info("Number of times the Contact form is submitted : "+contactService.getCounter());
+        return "redirect:/contact";
     }
 }
--- src/main/java/com/example/eazyschool/service/ContactService.java
+++ src/main/java/com/example/eazyschool/service/ContactService.java
@@ -3,14 +3,36 @@ package com.example.eazyschool.service;
 import com.example.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Slf4j
 @Service
+@ApplicationScope
 public class ContactService {
-    public boolean saveMessageDetails(Contact contact) {
+
+    private int counter = 0;
+
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
-        System.out.println(contact);
+        //TODO - Need to persist the data into the DB table
         log.info(contact.toString());
         return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
